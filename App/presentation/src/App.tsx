5d6ca9e24d4ac40d7293082f12bf7a15c790c4e8
ucdale <ucdale@gmail.com>
2024-12-22 10:57:38 +0100

    Prova con MUI installato

--- App/presentation/src/App.tsx
+++ App/presentation/src/App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import './App.css';
 import Logo from './Logo';
+import Button from '@mui/material/Button';
 
 const App = () => {
+
+  const goToReactGuide = useCallback(() => {
+    window.location.replace("https://reactjs.org");
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +16,7 @@ const App = () => {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button variant="contained" onClick={goToReactGuide}  className="App-link" >Learn React</Button>
       </header>
     </div>
   );
