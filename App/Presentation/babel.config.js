a607b00f462ef2ed384de9f57b2a8f211335199c
ucdale <ucdale@gmail.com>
2024-12-30 16:25:14 +0100

    Revert conversione a ES module che non fa andare i test nel workflow di github

--- App/Presentation/babel.config.js
+++ App/Presentation/babel.config.js
@@ -1,5 +1,5 @@
-// filepath: /c:/Users/ucdale/Documents/GitHub/TbF_17/App/Presentation/babel.config.js
-export default {
+// eslint-disable-next-line
+module.exports = {
   presets: [
     '@babel/preset-env',
     '@babel/preset-react',
